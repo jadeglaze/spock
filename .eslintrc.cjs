c6b03de040bc082389c756434b580f9c5ad50d78
jadeglaze <jade.glaze@gmail.com>
2024-08-07 21:06:24 -0400

    one more build warn

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,7 @@ const config = {
     "plugin:@typescript-eslint/stylistic-type-checked"
   ],
   "rules": {
+    "@next/next/no-img-element": "off",
     "@typescript-eslint/no-unsafe-assignment": "off",
     "@typescript-eslint/no-unsafe-call": "off",
     "@typescript-eslint/no-unsafe-member-access": "off",
